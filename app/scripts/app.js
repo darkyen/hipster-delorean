5d307ec4655048a5898cac4d885a906a6423a70d
Abhishek Hingnikar <abhishek@hingnikar.com>
2014-11-29 02:54:37 +0530

    Got hello world working YAYYYYYYYYYYY!!!!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,12 +9,12 @@ class App{
 	constructor(router, appRoot){
 		this.router = router;
 		this.appRoot = appRoot;
-		this.router.run(this.routerInitialized);
+		this.router.run(this.routerInitialized.bind(this));
 		this.http = Promise.promisifyAll(request);
 	}
 	
 	routerInitialized (Handler, state){
-		console.log(state);
+		console.log(this.appRoot.toString());
 		React.render(<Handler />, this.appRoot);
 	}
 }
